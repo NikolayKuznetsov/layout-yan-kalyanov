aabb4828fc2460a507b1a6613725e5157495d60b
Nikolay Kuznesov <nikolay.kuznesov@mainsoft.by>
2017-02-08 17:42:01 +0300

    add animate

--- public/js/main.js
+++ public/js/main.js
@@ -17,7 +17,33 @@ $(document).ready(function () {
         return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
     }
 
+    // функция, которая по заданному селектору
+// найдет соответствующие ему элементы, которые
+// при этом попадают в видимую область окна
+    function inWindow(s){
+        var scrollTop = $(window).scrollTop();
+        var windowHeight = $(window).height();
+        var currentEls = $(s);
+        var result = [];
+        currentEls.each(function(){
+            var el = $(this);
+            var offset = el.offset();
+            if(scrollTop <= offset.top && (el.height() + offset.top) < (scrollTop + windowHeight))
+                result.push(this);
+        });
+        return $(result);
+    }
+
+
+
     $(window).scroll(function () {
+
+        var boxesInWindow3 = inWindow("#block-3");
+        boxesInWindow3.addClass('animate-bg');
+
+        var boxesInWindow5 = inWindow("#block-5");
+        boxesInWindow5.addClass('animate-bg');
+
         if (come("#block-1")) {
             $('.menu-container ul.list-menu li').removeClass('active');
             $('.menu-container ul.list-menu li:nth-child(1)').addClass('active');
